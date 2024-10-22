4e8cf1e9e0031587940f75a5012cc6167d98d8ab
rjohn05-dc <rohanmathewjohn@gmail.com>
2024-10-21 22:34:46 -0700

    updated switch.js
    finished code blocks 1-6, will do 7-10 tomorrow

--- switch.js
+++ switch.js
@@ -1,31 +1,9 @@
 // Nested If-Else 1
-let weather = 'rainy';
 
-if (weather === 'sunny') {
 
-    console.log('Wear sunglasses');
+let weather="rainy"
 
-} else if (weather === 'rainy') {
-
-    console.log('Take an umbrella');
-
-} else if (weather === 'cloudy') {
-
-    console.log('Might need a light jacket');
-
-} else if (weather === 'snowy') {
-
-    console.log('Wear a coat and gloves');
-
-} else {
-
-    console.log('Weather is unpredictable');
-
-}
-
-let weather1="rainy"
-
-switch(weather1){
+switch(weather){
     case sunny:
         console.log("Wear sunglasses")
         break;
@@ -44,3 +22,109 @@ switch(weather1){
 
 
 // Nested If-Else 2
+
+
+let day=3
+
+switch(day){
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    default:
+        console.log("Sunday")
+}
+
+// Nested If-Else 3
+
+let trafficLight="green"
+
+switch(trafficLight){
+    case red:
+        console.log("Stop")
+        break;
+    case yellow:
+        console.log("Slow Down")
+        break;
+    case green:
+        console.log("Go")
+        break;
+    default:
+        console.log("Proceed with caution")
+}
+
+// Nested If-Else 4
+let score=75
+
+switch(score){
+    case 90:
+        console.log("A")
+        break;
+    case 80:
+        console.log("B")
+        break;
+    case 70:
+        console.log("C")
+        break;
+    case 60:
+        console.log("D")
+        break;
+    default:
+        console.log("F")
+}
+
+// Nested If-Else 5
+
+let size="L"
+
+switch(size){
+    case S:
+        console.log("Small")
+        break;
+    case M:
+        console.log("Medium")
+        break;
+    case L:
+        console.log("Large")
+        break;
+    case XL:
+        console.log("Extra Large")
+        break;
+    default:
+        console.log("Unknown Size")
+}
+
+// Nested If-Else 6
+
+switch(browser){
+    case Chrome:
+        console.log("Fast and Popular")
+        break;
+    case FireFox:
+        console.log("Privacy-focused")
+        break;
+    case Safari:
+        console.log("Best for Apple devices")
+        break;
+    case Edge:
+        console.log("Windows default bowser")
+        break;
+    default:
+        console.log("Other Browser")
+}
+
+//  Nested If-Else 7
