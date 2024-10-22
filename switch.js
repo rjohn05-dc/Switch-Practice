077e6b4333593d6b049846de34dc152848a11045
rjohn05-dc <rohanmathewjohn@gmail.com>
2024-10-21 18:20:46 -0700

    updated switch.js
    finished the first code block

---
+++ switch.js
@@ -0,0 +1,46 @@
+// Nested If-Else 1
+let weather = 'rainy';
+
+if (weather === 'sunny') {
+
+    console.log('Wear sunglasses');
+
+} else if (weather === 'rainy') {
+
+    console.log('Take an umbrella');
+
+} else if (weather === 'cloudy') {
+
+    console.log('Might need a light jacket');
+
+} else if (weather === 'snowy') {
+
+    console.log('Wear a coat and gloves');
+
+} else {
+
+    console.log('Weather is unpredictable');
+
+}
+
+let weather1="rainy"
+
+switch(weather1){
+    case sunny:
+        console.log("Wear sunglasses")
+        break;
+    case rainy:
+        console.log("Take an umbrella")
+        break;
+    case cloudy:
+        console.log("Might need a light jacket")
+        break;
+    case snowy:
+        console.log("Wear a coat and gloves")
+        break;
+    default:
+        console.log("Weather is unpredictable")
+}
+
+
+// Nested If-Else 2
